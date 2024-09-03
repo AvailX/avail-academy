addb5fcdcd3a38a2b6f81d36272d3122c1018ca1
ChibuezeSamObisike <samobisike@gmail.com>
2024-09-03 13:27:23 +0100

    chore: push code for layout

---
+++ src/hooks/useMediaQueries.ts
@@ -0,0 +1,11 @@
+import { useTheme, useMediaQuery } from '@mui/material';
+
+export const useMediaQueries = () => {
+  const theme = useTheme();
+
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isTablet = useMediaQuery(theme.breakpoints.down('md'));
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+
+  return { isMobile, isTablet, isDesktop };
+};
---
+++ src/theme/index.tsx
@@ -0,0 +1,5 @@
+import { createTheme } from '@mui/material';
+
+// ! Note: This would help to make a lot of things reusable when the design is ready will set it up better
+const theme = createTheme({});
+export default theme;
